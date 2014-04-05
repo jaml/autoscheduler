eca2b75145771b2a105c30e8a906ef5c73377f02
Jacqueline Lo <jaml@ucla.edu>
2014-04-04 23:25:46 -0700

    secret keys template

--- secret.php
+++ secret.php
@@ -2,5 +2,8 @@
 
 // Don't push API keys to the public repo!
 
+$gapi_client_id = '';
+$gapi_client_secret = '';
+
 ?>
 
--- src/gcal.php
+++ src/gcal.php
@@ -5,4 +5,6 @@ include_once '../secret.php';
 require_once '../lib/gapi/src/Google/Client.php';
 require_once '../lib/gapi/src/Google/Service/Calendar.php';
 
+$redirect = 'http://localhost:8080/gcal.php'; // change this later
+
 ?>
